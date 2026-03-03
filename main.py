3d5a026257f7e7478812dfc30ac38a01fe585740
ClaudiaACreatividad <clauliliana1963gmail.com>
2026-03-03 20:29:48 -0300

    corregir CORS para GitHub Pages

--- main.py
+++ main.py
@@ -13,7 +13,12 @@ app = FastAPI(title="Reserva de Aulas")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://claudiaacreativity.github.io",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "null"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
